59c96f8a4a38d6ce9c02b2db02bf23569a7cff68
Ali Hamza <alihamza1795.ah@gmail.com>
2020-05-26 01:21:46 +0530

    Changes in add

--- src/hello_world/Add.java
+++ src/hello_world/Add.java
@@ -4,7 +4,8 @@ public class Add {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print(5+5);
+		System.out.println(5+5);
+		System.out.println(10+10);
 	}
 
 }
